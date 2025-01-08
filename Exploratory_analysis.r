d842d06f5ddeed2e4dc8cd300a40e96e74aef8fd
John-Pierre Weideman <jpweideman@JohnPierresAir.lan>
2025-01-08 21:50:10 +0100

    Some more sentiment analysis

--- Exploratory_analysis.r
+++ Exploratory_analysis.r
@@ -17,7 +17,6 @@ sentiment_scores <- cleaned_df %>%
 # View the sentiment scores
 print(sentiment_scores)
 
-# Visualize the sentiment scores (average polarity)
 # Aggregate tokenized words back into sentences
 cleaned_df <- read.csv("Data/cleaned_df.csv", stringsAsFactors = FALSE)
 reconstructed_text <- cleaned_df %>%
@@ -27,6 +26,10 @@ reconstructed_text <- cleaned_df %>%
     .groups = "drop"
   )
 reconstructed_text
+# Save the reconstructed text
+write.csv(reconstructed_text, "Data/reconstructed_cleaned_df.csv", row.names = FALSE)
+
+# Visualize the sentiment scores (average polarity)
 # Sentiment analysis using qdap::polarity()
 sentiment_results <- reconstructed_text %>%
   rowwise() %>%
@@ -54,8 +57,6 @@ ggplot(sentiment_results, aes(x = Label, y = avg_polarity, fill = Label)) +
   )
 # Save the plot
 ggsave("Plots/sentiment_scores_plot.png", width = 10, height = 6, bg = "white")
-
-
 
 # Visualize the distribution of polarity with box plot
 # Sentiment analysis using qdap::polarity()
@@ -85,3 +86,28 @@ ggplot(sentiment_results, aes(x = Label, y = polarity_score, fill = Label)) +
   )
 # Save the box plot
 ggsave("Plots/sentiment_box_plot.png", width = 10, height = 6, bg = "white")
+
+# Perform sentiment analysis using the Bing lexicon for each category in the Label column
+# Read the reconstructed cleaned data
+cleaned_df <- read.csv("Data/reconstructed_cleaned_df.csv")
+category_sentiment <- cleaned_df %>%
+  unnest_tokens(word, full_text) %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(Label, word, sentiment, sort = TRUE) %>%
+  ungroup()
+# Visualize the top words contributing to sentiment for each category
+category_sentiment %>%
+  group_by(Label, sentiment) %>%
+  top_n(10) %>%
+  ungroup() %>%
+  mutate(word = reorder_within(word, n, Label)) %>%
+  ggplot(aes(word, n, fill = sentiment)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~Label + sentiment, scales = "free_y") +
+  scale_x_reordered() +
+  labs(y = "Contribution to sentiment",
+       x = NULL,
+       title = "Top Words Contributing to Sentiment by Category") +
+  coord_flip()
+# Save the plot
+ggsave("Plots/top_words_contributing_to_sentiment_by_category.png", width = 10, height = 6, bg = "white")
